d69afe1f4d4be675233caec4b1d2ea0634e07eb8
bryce <brycemcole@icloud.com>
2023-11-16 07:17:37 +0000

    1 step closer to account registration boys

--- server.js
+++ server.js
@@ -1,43 +1,62 @@
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
 const mysql = require("mysql2");
 const bcrypt = require("bcrypt");
 const app = express();
 const port = process.env.PORT || 8080; // Use the environment's port if provided.
 
+// Middleware to parse JSON bodies of requests with Content-Type: application/json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  host: "127.0.0.1",
+  user: "dev",
+  password: "##",
 });
 
 app.use(
   session({
     secret: "secret",
     store: new MySQLStore({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: "session_test",
+      host: "127.0.0.1",
+      user: "dev",
+      password: "##",
+      database: "core",
     }),
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true },
+    cookie: { secure: app.get("env") === "production" },
   })
 );
 
-app.use(express.urlencoded({ extended: true }));
+function currentTimestampInSQL() {
+  const now = new Date();
 
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0"); // Months start at 0 in JavaScript
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+// /register route
 app.post("/register", async (req, res) => {
+  //console.log(req.body);
   try {
     const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
+    const timeStamp = currentTimestampInSQL();
 
     const query =
-      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-    connection.execute(
-      query[(username, email, hashedPassword)],
+      "INSERT INTO users (username, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?)";
+    db.execute(
+      query[(username, email, hashedPassword, timeStamp, timeStamp)],
       (err, results) => {
         if (err) {
           console.error(err);
