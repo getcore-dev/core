57b5d2da5e144bc9fe7fdfe94af1757d39377ba6
bryce <brycemcole@icloud.com>
2023-11-16 02:13:21 +0000

    Refactored code to improve performance and readability.

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    try { 
-        const { username, email, password } = req.body;
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-        // check if user exists
-        let user = await User.findOne({ email });
-        if (user) { 
-            return res.status(400).json({ message: 'User already exists' });
-        }
+    // check if user exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-        // create user and save to database
-        user = new User({ username, email, password });
-        await user.save();
+    // create user and save to database
+    user = new User({ username, email, password });
+    await user.save();
 
-        res.status(201).json({ message: 'User created successfully' });
-    } catch (err) {
-        res.status(500).json({ message: err})
-    }  
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,39 +1,66 @@
-const express = require('express');
-const path = require('path');
-const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const path = require("path");
+const MySQLStore = require("express-mysql-session")(session);
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 const app = express();
 const port = process.env.PORT || 8080; // Use the environment's port if provided.
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
+app.use(
+  session({
+    secret: "secret",
+    store: new MySQLStore({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: "session_test",
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true },
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
-app.post('/register', async (req, res) => {
-try {
-    const { username, email, password } = req.body; 
+
+app.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    
-    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-    connection.execute(query [username, email, hashedPassword], (err, results) => { 
-    if (err) {
-        console.error(err);
-        return res.status(500).send('Error registering new user');
+
+    const query =
+      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    connection.execute(
+      query[(username, email, hashedPassword)],
+      (err, results) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Error registering new user");
         }
         // what to do if user logs in successfully
-        res.redirect('/login');
-        });
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("Internal server error");
-            }
-    });
-        
+        res.redirect("/login");
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // If you want to use a router for additional paths, you can include them here.
 // For example, app.use('/api', apiRoutes);
 
 // Catch-all for any request that doesn't match one above to send back `index.html`.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'core.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "core.html"));
 });
 
 app.listen(port, () => {
