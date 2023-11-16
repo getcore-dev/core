e86fb414bc1e07322bd990aa9db76cdf0c6d9fc1
bryce <brycemcole@icloud.com>
2023-11-15 20:18:08 -0500

    added registration logic

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = process.env.PORT || 8080; // Use the environment's port if provided.
+
+app.post('/register', async (req, res) => {
+try {
+    const { username, email, password } = req.body; 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+    connection.execute(query [username, email, hashedPassword], (err, results) => { 
+    if (err) {
+        console.error(err);
+        return res.status(500).send('Error registering new user');
+        }
+        // what to do if user logs in successfully
+        res.redirect('/login');
+        });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal server error");
+            }
+    });
+        
 
 app.use(express.static(path.join(__dirname, 'public')));
 
