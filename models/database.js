2c4395459e578d62180a0e5c7be163877c71ceed
brycemcole <brycemcole@icloud.com>
2023-11-21 02:09:10 -0500

    oops leaked db pass lol

--- models/database.js
+++ models/database.js
@@ -1,9 +1,15 @@
-// database.js
+require("dotenv").config();
+
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("core", "dev", "Socks!Gloves!4314", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 module.exports = sequelize;
