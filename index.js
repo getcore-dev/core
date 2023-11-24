876e90a0538687515c11b7e2ee11ac1c8f2df162
brycemcole <brycemcole@icloud.com>
2023-11-24 02:56:34 -0500

    CORE-20 reverted some changed idk

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require("express");
 const path = require("path");
 const compression = require("compression");
 const rateLimit = require("express-rate-limit");
-const lusca = require("lusca"); // Import lusca middleware
+const lusca = require("lusca");
 const fs = require("fs");
 const appConfig = require("./config/appConfig");
 const userRoutes = require("./routes/userRoutes");
 const sessionMiddleware = require("./middleware/session");
-const { isValidPath } = require("./utils/pathValidator");
-const navbar = fs.readFileSync(
-  path.join(__dirname, "/public", "navbar.html"),
-  "utf8"
-);
+
 const app = express();
 const port = appConfig.port;
+
+// Read the navbar.html file
+const navbar = fs.readFileSync(
+  path.join(__dirname, "public", "navbar.html"),
+  "utf8"
+);
 
 // Apply rate limiting as an early middleware to all requests
 app.use(rateLimit(appConfig.rateLimitSettings));
@@ -36,14 +38,32 @@ app.use(
 );
 
 // Use lusca CSRF protection middleware
-app.use(lusca.csrf()); // Add this line to enable CSRF protection
+app.use(lusca.csrf());
 
 // User specific routes
 app.use("/user", userRoutes);
 
 // Catch all route for serving front-end application
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "core.html"));
+  // Read the content of the specific page
+  const pagePath = path.join(
+    __dirname,
+    "public",
+    req.path === "/" ? "core.html" : req.path
+  );
+  if (fs.existsSync(pagePath) && path.extname(pagePath) === ".html") {
+    const content = fs.readFileSync(pagePath, "utf8");
+
+    // Replace a placeholder in your HTML file with the navbar content
+    const pageWithNavbar = content.replace(
+      "<!-- Navbar Placeholder -->",
+      navbar
+    );
+    res.send(pageWithNavbar);
+  } else {
+    // If the file doesn't exist or isn't an HTML file, send the default file
+    res.sendFile(path.join(__dirname, "public", "core.html"));
+  }
 });
 
 // Start the server
