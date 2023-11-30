5781d76dd4b72e7d0b75abc9bd98f7222ed43627
brycemcole <brycemcole@icloud.com>
2023-11-30 13:22:11 -0500

    testing the db with new azure connection

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,34 +15,39 @@ exports.checkSession = (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { username, password } = req.body;
+    const query =
+      "SELECT user_id, username, password_hash FROM users WHERE username = ?";
 
-    // Find the user by username using Sequelize
-    const user = await User.findOne({ where: { username } });
-    if (!user) {
-      return res.status(401).send("Incorrect username or password");
-    }
-
-    // Use bcrypt to compare the provided password with the hashed password stored in the database
-    const isMatch = await bcrypt.compare(password, user.password_hash);
-    if (!isMatch) {
-      return res.status(401).send("Incorrect username or password");
-    }
-
-    // Set the session userId and username with the user's ID and username
-    req.session.userId = user.user_id; // Store the user's ID in the session
-    req.session.username = user.username; // Store the username as well
-    console.log(
-      `user #${req.session.userId} ${req.session.username} has logged in`
-    );
-
-    console.log(`Session ID: ${req.session.id}`); // Redirect to the home page or dashboard as needed
-
-    req.session.save((err) => {
-      if (err) {
-        return res.send("err while saving session information");
+    db.query(query, [username], async (error, results) => {
+      if (error) {
+        return res.status(500).send("Internal server error");
       }
-      console.log(req.session.id);
-      return res.redirect("/");
+
+      const user = results[0];
+      if (!user) {
+        return res.status(401).send("Incorrect username or password");
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password_hash);
+      if (!isMatch) {
+        return res.status(401).send("Incorrect username or password");
+      }
+
+      req.session.userId = user.user_id;
+      req.session.username = user.username;
+      console.log(
+        `user #${req.session.userId} ${req.session.username} has logged in`
+      );
+
+      console.log(`Session ID: ${req.session.id}`); // Redirect to the home page or dashboard as needed
+
+      req.session.save((err) => {
+        if (err) {
+          return res.send("err while saving session information");
+        }
+        console.log(req.session.id);
+        return res.redirect("/");
+      });
     });
   } catch (error) {
     console.error(error);
@@ -55,24 +60,29 @@ exports.register = async (req, res) => {
     const { username, email, password, zipcode } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create the user using Sequelize
-    const newUser = await User.create({
-      username,
-      email,
-      password_hash: hashedPassword,
-      zip_code: zipcode,
-    });
+    const insertQuery =
+      "INSERT INTO users (username, email, password_hash, zip_code) VALUES (?, ?, ?, ?)";
 
-    // Set the session userId and username with the new user's ID and username
-    req.session.userId = newUser.user_id; // Store the new user's ID in the session
-    req.session.username = newUser.username; // Store the new username as well
+    db.query(
+      insertQuery,
+      [username, email, hashedPassword, zipcode],
+      (error, results) => {
+        if (error) {
+          if (error.code === "ER_DUP_ENTRY") {
+            return res.status(400).send("Username or email already exists");
+          }
+          console.error(error);
+          return res.status(500).send("Internal server error");
+        }
 
-    // Redirect to the home page or dashboard as needed
-    res.redirect("/"); // Redirecting also sends a 200 OK by default
+        const newUserId = results.insertId;
+        req.session.userId = newUserId;
+        req.session.username = username;
+        // Redirect to the home page or dashboard as needed
+        res.redirect("/"); // Redirecting also sends a 200 OK by default
+      }
+    );
   } catch (error) {
-    if (error.name === "SequelizeUniqueConstraintError") {
-      return res.status(400).send("Username or email already exists");
-    }
     console.error(error);
     res.status(500).send("Internal server error");
   }
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const app = express();
+import fetch from "node-fetch";
 const port = process.env.PORT || 8080;
+const GITHUB_API_URL = "https://api.github.com/repos/brycemcole/CORE/commits"; // Replace with your repo details
 
 app.set("view engine", "ejs");
 app.set("trust proxy", 1); // Trust first proxy for session security
@@ -37,9 +39,18 @@ app.use("/user", require("./routes/userRoutes"));
 app.use("/learning", require("./routes/learningRoutes"));
 app.use("/api", require("./routes/apiRoutes"));
 
-// Removed redundant endpoints and consolidated static rendering
-
 // Homepage route
+
+app.get("/github-updates", async (req, res) => {
+  try {
+    const response = await fetch(GITHUB_API_URL);
+    const data = await response.json();
+    res.json(data); // Send the data to the frontend
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching data from GitHub" });
+  }
+});
+
 app.get("/", (req, res) => {
   res.render("communities", {
     pagePath: "communities",
