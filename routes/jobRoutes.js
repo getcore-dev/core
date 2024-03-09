f69d9cfffa2732ed54b22d1c9aa2afe938fef00d
brycemcole <brycemcole@icloud.com>
2024-03-09 16:26:02 -0500

    visual updates

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const jobQueries = require("../queries/jobQueries");
+const {
+  checkAuthenticated,
+  checkNotAuthenticated,
+} = require("../middleware/authMiddleware");
 
-router.get("/create", async (req, res) => {
+router.get("/create", checkAuthenticated, async (req, res) => {
   try {
     const skills = await jobQueries.getSkills();
-    res.render("create-job.ejs", { skills });
+    res.render("create-job.ejs", { skills, user: req.user });
   } catch (err) {
     console.error("Error fetching job postings:", err);
     res.status(500).send("Error fetching job postings");
