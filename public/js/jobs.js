3b9ccf2db6f61891fcfe7734a4d2ba7a2a7f2acf
bryce <brycemcole@icloud.com>
2024-03-10 10:30:12 -0400

    Update jobs.js

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", () => {
         <p class="company-name">${job.company_name}</p>
           <h3 class="job-title">${job.title}</h3>
           <div class="job-posting- flairs">
-          <span class="job-flair" id="location-flair"><p class="job-location">${job.location}</p>
-          <span class="job-flair" id="pay-flair"><p class="salary-range">$${Math.floor((job.salary + job.salary_max) / 2 / 1000)}k</p><span>
+          <span class="job-flair" id="location-flair"><p class="job-location">${job.location}</p></span>
+          <span class="job-flair" id="pay-flair"><p class="salary-range">$${Math.floor((job.salary + job.salary_max) / 2 / 1000)}k</p></span>
           <span class="job-flair"><p class="experience-level">${job.experienceLevel}</p></span>
           <span class="job-flair"><p class="posted-date">${formatDate(job.postedDate)}</p></span>
           </div>
