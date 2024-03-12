7d79814cd11cfdaaee92b66875ddedcecb980e08
brycemcole <brycemcole@icloud.com>
2024-03-12 15:43:07 -0400

    updated jobs page

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -13,17 +13,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
         jobElement.innerHTML = `
         <div class="job-preview">
-        <div class="company-logo">
-          <img src="${job.company_logo}" alt="${job.company_name} logo" />
-        </div>
         <div class="job-info">
-        <p class="company-name">${job.company_name}</p>
+          <div class="company-info">
+          <img src="${job.company_logo}" alt="${job.company_name} logo" /> 
+          <p class="company-name">${job.company_name}</p>
+        </div>
           <h3 class="job-title">${job.title}</h3>
           <div class="job-posting- flairs">
-          <span class="job-flair" id="location-flair"><p class="job-location">${job.location}</p></span>
-          <span class="job-flair" id="pay-flair"><p class="salary-range">$${Math.floor((job.salary + job.salary_max) / 2 / 1000)}k</p></span>
-          <span class="job-flair"><p class="experience-level">${job.experienceLevel}</p></span>
-          <span class="job-flair"><p class="posted-date">${formatDate(job.postedDate)}</p></span>
+          <span class="job-flair" id="location-flair"><p class="job-location">${
+            job.location
+          }</p></span>
+          <span class="job-flair" id="pay-flair"><p class="salary-range">$${Math.floor(
+            (job.salary + job.salary_max) / 2 / 1000
+          )}k</p></span>
+          <span class="job-flair"><p class="experience-level">${
+            job.experienceLevel
+          }</p></span>
+          <span class="job-flair"><p class="posted-date">${formatDate(
+            job.postedDate
+          )}</p></span>
           </div>
         </div>
       </div>
@@ -39,8 +47,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function formatDate(dateString) {
   const date = new Date(dateString);
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
   const year = date.getFullYear();
   return `${month}/${day}/${year}`;
 }
