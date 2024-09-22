e9c520a54495beaf5a22dcd435335949ae3c1565
brycemcole <brycemcole@icloud.com>
2024-09-21 20:29:57 -0400

    test

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(
   session({
     store: new MSSQLStore(dbConfig),
