3927fb4006db3c4a26e06b24ed91b730180d6c22
brycemcole <brycemcole@icloud.com>
2024-11-02 09:59:01 -0400

    pptr

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+    // Download Chrome (default `skipDownload: false`).
+    chrome: {
+      skipDownload: false,
+    },
+    // Download Firefox (default `skipDownload: true`).
+    firefox: {
+      skipDownload: false,
+    },
+  };
